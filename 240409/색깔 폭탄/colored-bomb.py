5e6c7dc529de2affd9cb36e6c8c9dc73b4701dfc
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2024-04-09 00:50:40 +0900

    Title: 색깔 폭탄, Time: 366ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240409/색깔 폭탄/colored-bomb.py
@@ -0,0 +1,118 @@
+from collections import deque
+import copy
+
+n, m = map(int, input().split())
+graph = []
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+for _ in range(n) :
+    graph.append(list(map(int, input().split())))
+
+# 반시계 90도 회전
+def rotation_reverse_90() :
+    global graph
+
+    temp = [[0]*n for _ in range(n)]
+
+    for i in range(n) :
+        for j in range(n) :
+            temp[n-1-j][i] = graph[i][j]
+
+    graph = temp
+
+def find(a, b, visit) :
+    q = deque()
+    value = graph[a][b]
+    q.append([a, b])
+    visit[a][b] = True
+
+    path = [[a, b]]
+    # 빨간 폭탄 위치 임시체크용
+    red = []
+    while q :
+        x, y = q.popleft()
+        for i in range(4) :
+            mx = x + dx[i]
+            my = y + dy[i]
+            if 0 <= mx < n and 0 <= my < n :
+                if graph[mx][my] == value and visit[mx][my] == False :
+                    visit[mx][my] = True
+                    q.append([mx, my])
+                    path.append([mx, my])
+                elif graph[mx][my] == 0 :
+                    if [mx, my] not in red :
+                        red.append([mx, my])
+                        q.append([mx, my])
+
+
+    return path, red
+
+# 폭탄 터트리기
+def letsboom(boom, red) :
+    global answer
+
+    answer += (len(boom)+len(red))**2
+
+    for x, y in boom :
+        #터지고 빈공간 -2
+        graph[x][y] = -2
+
+    for x, y in red :
+        graph[x][y] = -2
+
+
+def findbomb() :
+    # 행 높고 열 낮은 우선순위
+    visited = [[False]*n for _ in range(n)]
+
+    boom = []
+    red_boom = []
+
+    for i in range(n-1, -1, -1) :
+        for j in range(n) :
+            if graph[i][j] >= 1 :
+                path, red = find(i, j, visited)
+                if len(path) + len(red) > len(boom) + len(red_boom) :
+                    boom = path
+                    red_boom = red
+                elif len(path) + len(red) == len(boom) + len(red_boom) and len(red) > len(red_boom) :
+                    boom = path
+                    red_boom = red
+
+    if len(boom) + len(red_boom) >= 2 :
+        letsboom(boom, red_boom)
+
+        return True
+    # 터트릴 것 못찾음
+    return False
+
+def gravity() :
+    global graph
+    temp = copy.deepcopy(graph)
+
+    for j in range(n) :
+        for i in range(n-1, 0, -1) :
+            if temp[i][j] == -2 and temp[i-1][j] != -1 :
+                temp[i][j], temp[i-1][j] = temp[i-1][j], temp[i][j]
+
+    if temp != graph :
+        graph = temp
+        gravity()
+
+
+
+answer = 0
+while True :
+    # 폭탄 터트릴께 있으면 터트리고 점수 얻음
+    if findbomb() == False :
+        break
+    # 터트렸으니 중력
+    gravity()
+    # 반시계 90도 회전
+    rotation_reverse_90()
+    # 다시 중력
+    gravity()
+
+print(answer)
