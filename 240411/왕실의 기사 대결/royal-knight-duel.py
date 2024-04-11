78ebbc36d6ba10dfbebc15f22adb3684162eed16
Jaehyun Park / 박재현 <48269869+jaehyun230@users.noreply.github.com>
2024-04-11 10:54:46 +0900

    Title: 왕실의 기사 대결, Time: 207ms, Memory: 27MB, Status: Passed - CodeTree

--- 240411/왕실의 기사 대결/royal-knight-duel.py
+++ 240411/왕실의 기사 대결/royal-knight-duel.py
@@ -1,159 +1,126 @@
-L, N, Q = map(int, input().split())
+from collections import deque
 
+# 체스판 크기, 기사 수, 명령어 수
+N, M, Q = map(int, input().split())
+# 게임판
 graph = []
-
+# 기사 정보 저장
 knights = []
-# 기사 살아있나 확인
-alive_knights = [True] * N
-
-knights_score = [0] * N
-
-# -1 이 빈칸 그다음부턴 idx로 기사 번호
-knights_graph = [[-1]*L for _ in range(L)]
-
-# 위 오 아 왼 (명령 방향)
+# 기사 위치 그래프
+knights_graph = [[-1] * N for _ in range(N)]
+# 기사 생존 여부 체크
+knights_alive = [True] * M
+# 기사 점수 저장
+knights_score = [0] * M
+# 이동 방향 - 위 오른 아래 왼
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-move_sets = set()
-
-# 나이트 그래프에 기사들 있는지 체크 그림 그리기
+# 기사 판 업데이트
 def knights_init() :
     global knights_graph
 
-    knights_graph = [[-1] * L for _ in range(L)]
+    knights_graph = [[-1] * N for _ in range(N)]
 
-    for knight in knights :
-        x, y, h, w, k, idx = knight
-
-        # 죽은 기사 경우 패스
-        if alive_knights[idx] == False :
+    for idx, knight in enumerate(knights) :
+        # 죽은 기사의경우 판 데이터 업데이트 반영 x
+        if knights_alive[idx] == False :
             continue
+        cr, cc, ch, cw, ck = knight
 
-        for i in range(x, x+h) :
-            for j in range(y, y+w) :
+        for i in range(cr, cr+ch) :
+            for j in range(cc, cc+cw) :
                 knights_graph[i][j] = idx
 
-def trap_check(x, y, h, w, k, d, idx) :
-    global alive_knights
-    global knights
-
-    # 트랩 갯수 카운트
-    count = 0
-    for i in range(x, x+h) :
-        for j in range(y, y+w) :
-            if graph[i][j] == 1 :
-                count +=1
-    # 함정이 체력이상이면 죽음 -> 해당위치 기사 없다고 업데이트 필요
-    if k <= count :
-        alive_knights[idx] = False
-
-    else :
-        # 체력 깎기
-        k = k - count
-        # 점수 증가
-        knights_score[idx] += count
-
-        # 해당 기사 정보 업데이트
-        knights[idx] = [x, y, h, w, k, idx]
-
-def move(x, y, h, w, k, d, idx, first_check) :
-    global knights
-    global alive_knights
-    global move_sets
-
-    mx = x + dx[d]
-    my = y + dy[d]
-
-    # 기사 높이
-    for i in range(mx, mx+h) :
-        for j in range(my, my+w) :
-            # 가고자 하는곳이 격자 안이고 벽이 아닌 경우
-            if 0 <= i < L and 0 <= j < L and graph[i][j] != 2 :
-
-                # 해당 위치에 자기 자신이 아닌 기사가 있는 경우
-                if knights_graph[i][j] != - 1 and knights_graph[i][j] != idx and knights_graph[i][j] not in move_sets:
-
-                    # print("다른 n번 기사가 있어요 밀칠께요", knights_graph[i][j])
-                    # 다른 n 번 기사 번호
-                    idx2 = knights_graph[i][j]
-                    x2, y2, h2, w2, k2, idx2 = knights[idx2]
-                    # 해당 나이트 밀었던적 있다고 기록
-                    move_sets.add(knights_graph[i][j])
-                    # 다른 기사야 너도 움직여라
-                    check =  move(x2, y2, h2, w2, k2, d, idx2, False)
-                    if check == False :
-                        # print(i, j, w2)
-                        # print("idx에게 idx2 안밀려요", idx, idx2, k2)
-                        # print("위치는 여기에요", x2, y2, d)
-                        return False
-
-            else :
-
-                # print("격자 밖이거나 벽이라 이동 불가 명령이행 실패")
-                return False
-
-    #이동 가능
-    # 함정 점수 계산 해야함
-    if first_check == False :
-        trap_check(mx, my, h, w, k, d, idx)
-    else :
-        # 첫 기사는 함정 점수 계산안해서 거기거 업데이트 안했음 -> 나이트 그래프도 추가 업데이트 필요
-        knights[idx] = [mx, my, h, w, k, idx]
-    return True
-
-def attack(num, d) :
-    global move_sets
-
-
-    global alive_knights
-    global knights_graph
-    global knights
+# 왕 움직여라 명령어 수행
+def attack(knight_num, move_d) :
+    global knights_alive
     global knights_score
+    global knights
 
-    temp_live = alive_knights
-    temp_graph = knights_graph
-    temp_knights = knights
-    temp_knights_score = knights_score
+    q = deque()
+    q.append(knight_num)
 
+    temp_damage = [0] * M
+    # 이동 한 기사 번호들 기록
     move_sets = set()
-    x, y, h, w, k, idx = knights[num]
-    move_sets.add(idx)
-    # 처음 밀려나가는 친구는 점수 계산 안하기 위함
+    move_sets.add(knight_num)
 
-    check = move(x, y, h, w, k, d, idx, True)
-    # 못움직이는 걸로 결론 나면 이전 행동 모두 취소
-    if check == False :
-        alive_knights = temp_live
-        knights_graph = temp_graph
-        knights = temp_knights
-        knights_score = temp_knights_score
+    check_graph = [[-1]*N for _ in range(N)]
+
+    while q :
+        num = q.popleft()
+        # 기사 정보 가져오기
+        cr, cc, ch, cw, ck = knights[num]
+
+        for i in range(cr, cr+ch) :
+            for j in range(cc, cc+cw) :
+                # 움직여서 위치할 포인트
+                mx = i + dx[move_d]
+                my = j + dy[move_d]
+
+                # 이동 하려는 위치가 판 내부이고 벽이 아니어야함
+                if 0 <= mx < N and 0 <= my < N and graph[mx][my] != 2 :
+
+                    check_graph[mx][my] = num
+                    # 움직이는 위치가 기사가 있고 연쇄작용 큐에 안들어간 기사인 경우
+                    if knights_graph[mx][my] != -1 and knights_graph[mx][my] not in move_sets :
+                        move_sets.add(knights_graph[mx][my])
+                        q.append(knights_graph[mx][my])
+                    # 만약 이동되어지는 위치가 함정인 경우 데미지 정보저장 -> 추후 초기 기사는 데미지 0 초기화필요
+                    if graph[mx][my] == 1 :
+                        temp_damage[num] +=1
+
+                # 이동 불가능 한 경우
+                else :
+                    return
+
+    # 다 이동할 수 있는 경우 작동
+    # 초기 기사는 데미지 받지 않음
+    temp_damage[knight_num] = 0
+    for kid in move_sets :
+
+        nr, nc, nh, nw, nk = knights[kid]
+        # 만약 체력 이상의 데미지를 받은 경우
+        if nk <= temp_damage[kid] :
+            knights_alive[kid] = False
+            continue
+        # 체력보다 덜 맞은 경우 점수 흑득
+        knights_score[kid] += temp_damage[kid]
+        # 함정에 깎인 체력 값도 변경
+        nk -= temp_damage[kid]
+
+        # 기사 이동한 정보로 업데이트
+        change_r = nr + dx[move_d]
+        change_c = nc + dy[move_d]
+        # 기사 정보 업데이트
+        knights[kid] = [change_r, change_c, nh, nw, nk]
 
 
-
-
-for _ in range(L) :
+# 체스판 입력 - 0 빈칸 1 함정 2 벽
+for _ in range(N) :
     graph.append(list(map(int, input().split())))
 
-for idx in range(N) :
+# 기사의 위치 좌표 입력
+for _ in range(M) :
     r, c, h, w, k = map(int, input().split())
-    knights.append([r-1, c-1, h, w, k, idx])
+    # 실제 보드는 크기가 작아서 바꿈
+    knights.append([r-1, c-1, h, w, k])
 
+# 게임 시작 전 기사 그래프 정보 삽입
 knights_init()
-
+# 왕의 명령에 대한 정보
 for _ in range(Q) :
-    #해당 기사번호에게 공격방향 명령어
-    knight_num, attack_d = map(int, input().split())
-
-    # 해당 기사가 살아있어야 명령 수행
-    if alive_knights[knight_num-1] == True :
-        attack(knight_num-1, attack_d)
-    # 나이트 그래프 다시 재구축
+    #명령받을 기사 번호 / 이동 방향
+    command_id, direction = map(int, input().split())
+    # 해당 기사가 살아있으면 수행
+    if knights_alive[command_id-1] == True :
+        attack(command_id-1, direction)
     knights_init()
 
-# 살아 있는 친구들 점수 계산
 answer = 0
-for i in range(N) :
-    if alive_knights[i] == True :
-        answer += knights_score[i]
+for value in range(M) :
+    if knights_alive[value] == True :
+        answer += knights_score[value]
+
 print(answer)
